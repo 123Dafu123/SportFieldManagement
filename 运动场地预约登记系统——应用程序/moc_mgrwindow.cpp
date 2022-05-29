35e3e2895177fe6d73920fe98d1aeb45e43aab61
123Dafu123 <100153552+123Dafu123@users.noreply.github.com>
2022-05-29 19:54:18 +0800

    Add files via upload

---
+++ 运动场地预约登记系统——应用程序/moc_mgrwindow.cpp
@@ -0,0 +1,167 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mgrwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.2.4)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../Sport/mgrwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mgrwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.2.4. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MgrWindow_t {
+    const uint offsetsAndSize[26];
+    char stringdata0[220];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(offsetof(qt_meta_stringdata_MgrWindow_t, stringdata0) + ofs), len 
+static const qt_meta_stringdata_MgrWindow_t qt_meta_stringdata_MgrWindow = {
+    {
+QT_MOC_LITERAL(0, 9), // "MgrWindow"
+QT_MOC_LITERAL(10, 15), // "on_MinB_clicked"
+QT_MOC_LITERAL(26, 0), // ""
+QT_MOC_LITERAL(27, 17), // "on_CloseB_clicked"
+QT_MOC_LITERAL(45, 18), // "on_FeildRb_clicked"
+QT_MOC_LITERAL(64, 20), // "on_MgrInfoRb_clicked"
+QT_MOC_LITERAL(85, 15), // "on_AddB_clicked"
+QT_MOC_LITERAL(101, 17), // "on_SearhB_clicked"
+QT_MOC_LITERAL(119, 21), // "on_StatisticB_clicked"
+QT_MOC_LITERAL(141, 18), // "on_ModfiyB_clicked"
+QT_MOC_LITERAL(160, 18), // "on_DeleteB_clicked"
+QT_MOC_LITERAL(179, 17), // "on_FinshB_clicked"
+QT_MOC_LITERAL(197, 22) // "on_InfoT_returnPressed"
+
+    },
+    "MgrWindow\0on_MinB_clicked\0\0on_CloseB_clicked\0"
+    "on_FeildRb_clicked\0on_MgrInfoRb_clicked\0"
+    "on_AddB_clicked\0on_SearhB_clicked\0"
+    "on_StatisticB_clicked\0on_ModfiyB_clicked\0"
+    "on_DeleteB_clicked\0on_FinshB_clicked\0"
+    "on_InfoT_returnPressed"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MgrWindow[] = {
+
+ // content:
+      10,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      11,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,   80,    2, 0x08,    1 /* Private */,
+       3,    0,   81,    2, 0x08,    2 /* Private */,
+       4,    0,   82,    2, 0x08,    3 /* Private */,
+       5,    0,   83,    2, 0x08,    4 /* Private */,
+       6,    0,   84,    2, 0x08,    5 /* Private */,
+       7,    0,   85,    2, 0x08,    6 /* Private */,
+       8,    0,   86,    2, 0x08,    7 /* Private */,
+       9,    0,   87,    2, 0x08,    8 /* Private */,
+      10,    0,   88,    2, 0x08,    9 /* Private */,
+      11,    0,   89,    2, 0x08,   10 /* Private */,
+      12,    0,   90,    2, 0x08,   11 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MgrWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MgrWindow *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->on_MinB_clicked(); break;
+        case 1: _t->on_CloseB_clicked(); break;
+        case 2: _t->on_FeildRb_clicked(); break;
+        case 3: _t->on_MgrInfoRb_clicked(); break;
+        case 4: _t->on_AddB_clicked(); break;
+        case 5: _t->on_SearhB_clicked(); break;
+        case 6: _t->on_StatisticB_clicked(); break;
+        case 7: _t->on_ModfiyB_clicked(); break;
+        case 8: _t->on_DeleteB_clicked(); break;
+        case 9: _t->on_FinshB_clicked(); break;
+        case 10: _t->on_InfoT_returnPressed(); break;
+        default: ;
+        }
+    }
+    (void)_a;
+}
+
+const QMetaObject MgrWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_MgrWindow.offsetsAndSize,
+    qt_meta_data_MgrWindow,
+    qt_static_metacall,
+    nullptr,
+qt_incomplete_metaTypeArray<qt_meta_stringdata_MgrWindow_t
+, QtPrivate::TypeAndForceComplete<MgrWindow, std::true_type>
+, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>
+
+
+>,
+    nullptr
+} };
+
+
+const QMetaObject *MgrWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MgrWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MgrWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MgrWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 11)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 11;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 11)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 11;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
---
+++ 运动场地预约登记系统——源码/regform.h
@@ -0,0 +1,54 @@
+#ifndef REGFORM_H
+#define REGFORM_H
+
+#include <QWidget>
+#include <QPoint>
+#include <QMouseEvent>
+#include "sportdatabase.h"
+
+
+namespace Ui {
+class RegForm;
+}
+
+class RegForm : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit RegForm(QWidget *parent = nullptr);
+    ~RegForm();
+
+protected:
+    void mouseReleaseEvent(QMouseEvent *event);
+    void mouseMoveEvent(QMouseEvent *event);
+    void mousePressEvent(QMouseEvent *event);
+
+private slots:
+    void on_CloseB_clicked();
+
+    void on_MinB_clicked();
+
+    void on_Reg_clicked();
+
+    void on_Cancel_clicked();
+
+    void on_IdT_returnPressed();
+
+    void on_PswdT_returnPressed();
+
+    void on_NameT_returnPressed();
+
+    void on_TeleT_returnPressed();
+
+private:
+    bool isPressedWidget;
+    QPoint m_lastPos;
+
+    Ui::RegForm *ui;
+    SportDataBase DB;
+
+    void reg();
+};
+
+#endif // REGFORM_H
